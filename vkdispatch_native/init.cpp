91e7141dbca5823d449d64c384762ea20360d37e
Shahar Sandhaus <wiishahar@gmail.com>
2024-04-02 18:57:06 -0700

    Basic project setup code

---
+++ vkdispatch_native/init.cpp
@@ -0,0 +1,5 @@
+#include "init.h"
+
+int add_one(int x) {
+    return x + 61;
+}
---
+++ vkdispatch_native/init.h
@@ -0,0 +1,8 @@
+#ifndef SRC_INIT_H
+#define SRC_INIT_H
+
+int add_one(int x);
+
+// Add your code here
+
+#endif // INIT_H
