f50f3ac158d41d26b1e1853dfd2088a9205d7e82
Shahar Sandhaus <wiishahar@gmail.com>
2024-05-16 22:25:44 -0700

    Reimplemented descriptor sets in standard vulkan

--- vkdispatch/descriptor_set.py
+++ vkdispatch/descriptor_set.py
@@ -10,7 +10,7 @@ class DescriptorSet:
     def __init__(self, compute_plan_handle: int) -> None:
         self._handle = vkdispatch_native.descriptor_set_create(compute_plan_handle)
 
-    def bind_buffer(self, buffer: vd.Buffer, binding: int) -> None:
+    def bind_buffer(self, buffer: vd.Buffer, binding: int, offset: int = 0, range: int = 0) -> None:
         vkdispatch_native.descriptor_set_write_buffer(
-            self._handle, binding, buffer._handle
+            self._handle, binding, buffer._handle, offset, range
         )
--- vkdispatch_native/descriptor_set.cpp
+++ vkdispatch_native/descriptor_set.cpp
@@ -3,10 +3,32 @@
 struct DescriptorSet* descriptor_set_create_extern(struct ComputePlan* plan) {
     struct DescriptorSet* descriptor_set = new struct DescriptorSet();
     descriptor_set->plan = plan;
-    descriptor_set->descriptorSets = new VKLDescriptorSet*[plan->ctx->deviceCount];
+
+    descriptor_set->pools.resize(plan->ctx->deviceCount);
+    descriptor_set->sets.resize(plan->ctx->deviceCount);
+    
+    //descriptor_set->descriptorSets = new VKLDescriptorSet*[plan->ctx->deviceCount];
 
     for (int i = 0; i < plan->ctx->deviceCount; i++) {
-        descriptor_set->descriptorSets[i] = new VKLDescriptorSet(plan->pipelineLayouts[i], 0);
+        //descriptor_set->descriptorSets[i] = new VKLDescriptorSet(plan->pipelineLayouts[i], 0);
+
+        VkDescriptorPoolCreateInfo descriptorPoolCreateInfo;
+        memset(&descriptorPoolCreateInfo, 0, sizeof(VkDescriptorPoolCreateInfo));
+        descriptorPoolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+        descriptorPoolCreateInfo.maxSets = 1;
+        descriptorPoolCreateInfo.poolSizeCount = plan->pipelineLayouts[i]->m_descriptorPoolSizes[0].size();
+        descriptorPoolCreateInfo.pPoolSizes = plan->pipelineLayouts[i]->m_descriptorPoolSizes[0].data();
+
+        VK_CALL(vkCreateDescriptorPool(plan->ctx->devices[i]->handle(), &descriptorPoolCreateInfo, NULL, &descriptor_set->pools[i]));
+
+        VkDescriptorSetAllocateInfo descriptorSetAllocateInfo;
+        memset(&descriptorSetAllocateInfo, 0, sizeof(VkDescriptorSetAllocateInfo));
+        descriptorSetAllocateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+        descriptorSetAllocateInfo.descriptorPool = descriptor_set->pools[i];
+        descriptorSetAllocateInfo.descriptorSetCount = 1;
+        descriptorSetAllocateInfo.pSetLayouts = plan->pipelineLayouts[i]->descriptorSetLayouts(); //&m_layout->descriptorSetLayouts()[set];
+
+        VK_CALL(vkAllocateDescriptorSets(plan->ctx->devices[i]->handle(), &descriptorSetAllocateInfo, &descriptor_set->sets[i]));
     }
 
     return descriptor_set;
@@ -14,17 +36,37 @@ struct DescriptorSet* descriptor_set_create_extern(struct ComputePlan* plan) {
 
 void descriptor_set_destroy_extern(struct DescriptorSet* descriptor_set) {
     for (int i = 0; i < descriptor_set->plan->ctx->deviceCount; i++) {
-        delete descriptor_set->descriptorSets[i];
+        vkDestroyDescriptorPool(descriptor_set->plan->ctx->devices[i]->handle(), descriptor_set->pools[i], NULL);        
+        //delete descriptor_set->descriptorSets[i];
     }
 
-    delete[] descriptor_set->descriptorSets;
+    //delete[] descriptor_set->descriptorSets;
     delete descriptor_set;
 }
 
-void descriptor_set_write_buffer_extern(struct DescriptorSet* descriptor_set, unsigned int binding, void* object) {
+void descriptor_set_write_buffer_extern(struct DescriptorSet* descriptor_set, unsigned int binding, void* object, unsigned long long offset, unsigned long long range) {
     struct Buffer* buffer = (struct Buffer*)object;
 
     for (int i = 0; i < descriptor_set->plan->ctx->deviceCount; i++) {
-        descriptor_set->descriptorSets[i]->writeBuffer(binding, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, buffer->buffers[i], 0, VK_WHOLE_SIZE);
+        VkDescriptorBufferInfo buffDesc;
+        buffDesc.buffer = buffer->buffers[i]->handle();
+        buffDesc.offset = offset;
+        buffDesc.range = range == 0 ? VK_WHOLE_SIZE : range;
+
+        VkWriteDescriptorSet writeDescriptor;
+        memset(&writeDescriptor, 0, sizeof(VkWriteDescriptorSet));
+        writeDescriptor.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        writeDescriptor.dstSet = descriptor_set->sets[i];
+        writeDescriptor.dstBinding = binding;
+        writeDescriptor.dstArrayElement = 0;
+        writeDescriptor.descriptorCount = 1;
+        writeDescriptor.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+        writeDescriptor.pImageInfo = NULL;
+        writeDescriptor.pBufferInfo = &buffDesc;
+        writeDescriptor.pTexelBufferView = NULL;
+
+        vkUpdateDescriptorSets(descriptor_set->plan->ctx->devices[i]->handle(), 1, &writeDescriptor, 0, NULL);
+        
+        //descriptor_set->descriptorSets[i]->writeBuffer(binding, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, buffer->buffers[i], offset, range == 0 ? VK_WHOLE_SIZE : range);
     }
 }
