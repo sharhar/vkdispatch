e9b798b88aee281848691ab82fe056c9e6bdb62e
Shahar Sandhaus <wiishahar@gmail.com>
2024-07-31 14:18:27 -0700

    Removing other OSs for now, might come back to it later

--- vkdispatch/tests/test_fft.py
+++ vkdispatch/tests/test_fft.py
@@ -7,29 +7,29 @@ def make_random_complex_signal(shape):
     i = np.random.random(size=shape)
     return (r + i * 1j).astype(np.complex64)
 
-# def test_fft_1d():
-#     # Create a 1D buffer
-#     signal = make_random_complex_signal((50,))
+def test_fft_1d():
+    # Create a 1D buffer
+    signal = make_random_complex_signal((50,))
 
-#     test_line = vd.Buffer((signal.shape[0],), vd.complex64)
-#     test_line.write(signal)
+    test_line = vd.Buffer((signal.shape[0],), vd.complex64)
+    test_line.write(signal)
 
-#     # Perform an FFT on the buffer
-#     vd.fft(test_line)
+    # Perform an FFT on the buffer
+    vd.fft(test_line)
 
-#     assert np.allclose(test_line.read(0), np.fft.fft(signal), atol=0.00001)
+    assert np.allclose(test_line.read(0), np.fft.fft(signal), atol=0.00001)
 
-# def test_fft_2d():
-#     # Create a 2D buffer
-#     signal_2d = make_random_complex_signal((50, 50))
+def test_fft_2d():
+    # Create a 2D buffer
+    signal_2d = make_random_complex_signal((50, 50))
 
-#     test_img = vd.Buffer(signal_2d.shape, vd.complex64)
-#     test_img.write(signal_2d)
+    test_img = vd.Buffer(signal_2d.shape, vd.complex64)
+    test_img.write(signal_2d)
 
-#     # Perform an FFT on the buffer
-#     vd.fft(test_img)
+    # Perform an FFT on the buffer
+    vd.fft(test_img)
 
-#     assert np.allclose(test_img.read(0), np.fft.fft2(signal_2d), atol=0.0001)
+    assert np.allclose(test_img.read(0), np.fft.fft2(signal_2d), atol=0.0001)
 
 def test_fft_3d():
     # Create a 3D buffer
@@ -43,29 +43,29 @@ def test_fft_3d():
 
     assert np.allclose(test_img.read(0), np.fft.fftn(signal_3d), atol=0.01)
 
-# def test_ifft_1d():
-#     # Create a 1D buffer
-#     signal = make_random_complex_signal((50, ))
+def test_ifft_1d():
+    # Create a 1D buffer
+    signal = make_random_complex_signal((50, ))
 
-#     test_line = vd.Buffer((signal.shape[0],), vd.complex64)
-#     test_line.write(signal)
+    test_line = vd.Buffer((signal.shape[0],), vd.complex64)
+    test_line.write(signal)
 
-#     # Perform an IFFT on the buffer
-#     vd.ifft(test_line)
+    # Perform an IFFT on the buffer
+    vd.ifft(test_line)
     
-#     assert np.allclose(test_line.read(0), np.fft.ifft(signal) * np.prod(signal.shape), atol=0.00001)
+    assert np.allclose(test_line.read(0), np.fft.ifft(signal) * np.prod(signal.shape), atol=0.00001)
 
-# def test_ifft_2d():
-#     # Create a 2D buffer
-#     signal_2d = make_random_complex_signal((50, 50))
+def test_ifft_2d():
+    # Create a 2D buffer
+    signal_2d = make_random_complex_signal((50, 50))
 
-#     test_img = vd.Buffer(signal_2d.shape, vd.complex64)
-#     test_img.write(signal_2d)
+    test_img = vd.Buffer(signal_2d.shape, vd.complex64)
+    test_img.write(signal_2d)
 
-#     # Perform an IFFT on the buffer
-#     vd.ifft(test_img)
+    # Perform an IFFT on the buffer
+    vd.ifft(test_img)
 
-#     assert np.allclose(test_img.read(0), np.fft.ifft2(signal_2d) * np.prod(signal_2d.shape), atol=0.0001)
+    assert np.allclose(test_img.read(0), np.fft.ifft2(signal_2d) * np.prod(signal_2d.shape), atol=0.0001)
 
 def test_ifft_3d():
     # Create a 3D buffer
