51a73867a1d8c817de6eee4a699f0fd9481bff1f
Shahar Sandhaus <wiishahar@gmail.com>
2024-08-12 12:12:38 -0700

    Removing the simpler reduction test

--- vkdispatch/codegen/tests/test_reductions.py
+++ vkdispatch/codegen/tests/test_reductions.py
@@ -5,31 +5,31 @@ import numpy as np
 
 vd.make_context(use_cpu=True)
 
-def test_reductions_sum():
-    # Create a buffer
-    buf = vd.Buffer((1024,) , vd.float32)
+# def test_reductions_sum():
+#     # Create a buffer
+#     buf = vd.Buffer((1024,) , vd.float32)
 
-    # Create a numpy array
-    data = np.random.rand(1024).astype(np.float32)
+#     # Create a numpy array
+#     data = np.random.rand(1024).astype(np.float32)
 
-    # Write the data to the buffer
-    buf.write(data)
+#     # Write the data to the buffer
+#     buf.write(data)
 
-    @vc.map_reduce(
-            exec_size=lambda args: args.buffer.size, 
-            reduction=lambda x, y: x + y, 
-            reduction_identity=0
-    )
-    def sum_map(ind: Const[i32], buffer: Buff[f32]) -> f32:
-        return buffer[ind]
+#     @vc.map_reduce(
+#             exec_size=lambda args: args.buffer.size, 
+#             reduction=lambda x, y: x + y, 
+#             reduction_identity=0
+#     )
+#     def sum_map(ind: Const[i32], buffer: Buff[f32]) -> f32:
+#         return buffer[ind]
     
-    res_buf = sum_map(buf)
+#     res_buf = sum_map(buf)
 
-    # Read the data from the buffer
-    read_data = res_buf.read(0)
+#     # Read the data from the buffer
+#     read_data = res_buf.read(0)
 
-    # Check that the data is the same
-    assert np.allclose([data.sum()], [read_data[0]])
+#     # Check that the data is the same
+#     assert np.allclose([data.sum()], [read_data[0]])
 
 def test_mapped_reductions():
     # Create a buffer
