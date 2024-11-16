af6cf3c80282bba8389e0e400dc82d25d38b75d0
sharhar <wiishahar@gmail.com>
2024-11-16 00:57:27 +0000

    Fixed race condition in the stream worker

--- vkdispatch_native/src/work_queue.cpp
+++ vkdispatch_native/src/work_queue.cpp
@@ -10,7 +10,7 @@ WorkQueue::WorkQueue(int max_work_items, int max_programs) {
     running = true;
 
     for(int i = 0; i < max_work_items; i++) {
-        work_infos[i].dirty.store(false);
+        work_infos[i].dirty = false;
         work_infos[i].header = (struct WorkHeader*)malloc(sizeof(struct WorkHeader) + 16 * 1024);
         memset(work_infos[i].header, 0, sizeof(struct WorkHeader) + 16 * 1024);
         work_infos[i].header->array_size = 16 * 1024;
@@ -73,7 +73,7 @@ void WorkQueue::push(struct CommandList* command_list, void* instance_buffer, un
         int work_index = -1;
 
         for(int i = 0; i < this->work_info_count; i++) {
-            if(!this->work_infos[i].dirty.load()) {
+            if(!this->work_infos[i].dirty) {
                 work_index = i;
                 break;
             }
@@ -100,7 +100,7 @@ void WorkQueue::push(struct CommandList* command_list, void* instance_buffer, un
 
     work_infos[found_indicies[1]].program_index = found_indicies[0];
     work_infos[found_indicies[1]].stream_index = stream_index;
-    work_infos[found_indicies[1]].dirty.store(true);
+    work_infos[found_indicies[1]].dirty = true;
     work_infos[found_indicies[1]].state = WORK_STATE_PENDING;
     work_infos[found_indicies[1]].work_id = __work_id;
     __work_id += 1;
@@ -146,34 +146,24 @@ void WorkQueue::push(struct CommandList* command_list, void* instance_buffer, un
     if(work_size > 0)
         memcpy(&work_header[1], instance_buffer, work_size);
     
-    this->program_infos[found_indicies[0]].ref_count.fetch_add(1, std::memory_order_relaxed);
+    this->program_infos[found_indicies[0]].ref_count += 1;
 
     this->cv_push.notify_all();
 }
 
 bool WorkQueue::pop(struct WorkHeader** header, int stream_index) {
     std::unique_lock<std::mutex> lock(this->mutex);
-    
-    //auto start = std::chrono::high_resolution_clock::now();
 
     this->cv_push.wait(lock, [this, stream_index, header] () {
-        //auto end = std::chrono::high_resolution_clock::now();
-        //std::chrono::duration<double> elapsed = end - start;
-
         if(!running) {
             return true;
         }
-        
-        //if(elapsed.count() > 5) {
-        //    LOG_ERROR("Timed out waiting for work in queue");
-        //    return true;
-        //}
 
         int selected_index = -1;
         size_t work_id = ~((size_t)0);
 
         for(int i = 0; i < this->work_info_count; i++) {
-            if(this->work_infos[i].dirty.load() &&
+            if(this->work_infos[i].dirty &&
                this->work_infos[i].state == WORK_STATE_PENDING &&
                this->work_infos[i].work_id < work_id &&
                (this->work_infos[i].stream_index == stream_index ||
@@ -194,18 +184,12 @@ bool WorkQueue::pop(struct WorkHeader** header, int stream_index) {
         return true;
     });
 
-    //auto end = std::chrono::high_resolution_clock::now();
-    //std::chrono::duration<double> elapsed = end - start;
-
-    //if(elapsed.count() >= 5) {
-    //    return false;
-    //}
-
     return running;
 }
 
 void WorkQueue::finish(struct WorkHeader* header) {
-    program_infos[header->program_header->info_index].ref_count.fetch_sub(1, std::memory_order_relaxed);
-    work_infos[header->info_index].dirty.store(false);
+    std::unique_lock<std::mutex> lock(this->mutex);
+    program_infos[header->program_header->info_index].ref_count -= 1;
+    work_infos[header->info_index].dirty = false;
     this->cv_pop.notify_all();
 }
