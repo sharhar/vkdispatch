bdb4be4f18749d6d6ca51fa156d335c23dcea590
Shahar Sandhaus <wiishahar@gmail.com>
2025-03-02 23:07:17 -0800

    FFT test script

--- test.py
+++ test.py
@@ -3,31 +3,44 @@ import matplotlib.pyplot as plt
 import vkdispatch as vd
 import vkdispatch.codegen as vc
 
-signal = np.ones((16, 16), dtype=np.float32)
+import tqdm
+
+signal = np.ones((16, 4096, 4096), dtype=np.float32)
 #signal[:, 64:128] = 0
 
 signal_buffer = vd.asrfftbuffer(signal)
 
-vd.set_log_level(vd.LogLevel.INFO)
+cmd_stream = vd.CommandStream()
 
-vd.rfft(signal_buffer, padding=[(0, 0), (5, 7)])
+vd.rfft(signal_buffer, axes=[1, 2], cmd_stream=cmd_stream, padding=[(0, 0), (0, 0), (256, 4096 - 256)])
+vd.irfft(signal_buffer, axes=[1, 2], cmd_stream=cmd_stream)
 
-fft_singnal = signal_buffer.read_fourier(0)
+for _ in tqdm.tqdm(range(100)):
+    cmd_stream.submit(instance_count=10)
 
-real_signal = np.fft.irfft2(fft_singnal)
+#fft_singnal = signal_buffer.read_fourier(0)
+
+#print(fft_singnal.shape)
+
+#real_signal = np.fft.irfft2(fft_singnal[0])
+
+
+#real_buffer = signal_buffer.read_real(0)
+
+exit()
 
 fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 
-axs[0, 0].imshow(np.abs(fft_singnal))
+axs[0, 0].imshow(np.abs(fft_singnal[0]))
 axs[0, 0].set_title('Abs FFT')
 
-axs[0, 1].imshow(np.abs(np.fft.ifft(fft_singnal, axis=0)))
+axs[0, 1].imshow(np.abs(np.fft.ifft(fft_singnal[0], axis=0)))
 axs[0, 1].set_title('Angle FFT')
 
 axs[1, 0].imshow(np.abs(real_signal))
 axs[1, 0].set_title('Abs real')
 
-axs[1, 1].imshow(np.angle(real_signal))
+axs[1, 1].imshow(real_buffer[0])
 axs[1, 1].set_title('angle real')
 
 for ax in axs.flat:
