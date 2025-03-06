0ef71abea8979ea1c960ce27cbad12a5da229f72
Shahar Sandhaus <wiishahar@gmail.com>
2025-03-05 23:14:20 -0800

    Made some convolution tests

--- test.py
+++ test.py
@@ -5,52 +5,36 @@ import vkdispatch.codegen as vc
 
 import tqdm
 
-vd.initialize(debug_mode=True, log_level=vd.LogLevel.INFO)
+vd.initialize(debug_mode=True)
 
-signal_shape = (128, 128)
+buff_size = (1024, 1024)
+kernel_size = (1, buff_size[0], buff_size[1] // 2 + 1)
 
-x = np.linspace(-1, 1, signal_shape[0])
-y = np.linspace(-1, 1, signal_shape[1])
-x, y = np.meshgrid(x, y)
-d = np.sqrt(x*x + y*y)
-sigma, mu = 0.1, 0.0
-gaussian_signal = np.exp(-((d - mu)**2 / (2.0 * sigma**2)))
+buffer = vd.RFFTBuffer(buff_size)
+kernel = vd.Buffer(kernel_size, vd.complex64)
 
-square_signal = np.zeros(signal_shape)
-square_signal[32:96, 32:96] = 1
-
-convolved_signal = np.fft.ifftshift(np.fft.irfft2(
-    (np.fft.rfft2(np.fft.fftshift(gaussian_signal).astype(np.float32)).astype(np.complex64) 
-    * np.fft.rfft2(np.fft.fftshift(square_signal).astype(np.float32)).astype(np.complex64))
-    .astype(np.complex64)))
-
-gaussian_buffer = vd.asrfftbuffer(np.fft.fftshift(gaussian_signal))
-
-kernel_signal = np.fft.rfft2(np.fft.fftshift(square_signal)).reshape(1, 128, -1)
-
-square_buffer = vd.asbuffer(kernel_signal.astype(np.complex64))
+cmd_stream = vd.CommandStream()
 
 def clasical_convolv():
-    vd.rfft(gaussian_buffer)
-    vd.rfft(square_buffer)
+    vd.rfft(buffer, cmd_stream=cmd_stream)
 
     @vd.shader("out.size // 2")
     def convolve(out: vc.Buffer[vd.complex64], input: vc.Buffer[vd.complex64]):
         tid = vc.global_invocation().x
         out[tid] *= input[tid]
 
-    convolve(gaussian_buffer, square_buffer)
+    convolve(buffer, kernel, cmd_stream=cmd_stream)
 
-    vd.irfft(gaussian_buffer)
+    vd.irfft(buffer, cmd_stream=cmd_stream)
 
 #clasical_convolv()
 
-vd.convolve_2d(gaussian_buffer, square_buffer)
+vd.convolve_2d(buffer, kernel, cmd_stream=cmd_stream)
 
-real_buffer = np.fft.ifftshift(gaussian_buffer.read_real(0)) #/ np.prod(signal_shape)
+for _ in tqdm.tqdm(range(1000)):
+    cmd_stream.submit(100)
 
-plt.imshow(real_buffer) #[1:, 1:] - convolved_signal[:-1, :-1])
-plt.show()
+buffer.read()
 
 exit()
 
