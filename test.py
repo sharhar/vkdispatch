20b695d5b0949ba39506e2e49255af299c7a6da4
sharhar <wiishahar@gmail.com>
2025-03-10 15:47:59 -0700

    convolution debugging scripts

--- test.py
+++ test.py
@@ -33,65 +33,61 @@ side_len = 50
 signal_2d = (np.abs(make_gaussian_signal((side_len, side_len)))).astype(np.float32)
 kernel_2d = (np.abs(make_square_signal((side_len, side_len)))).astype(np.float32).reshape((1, side_len, side_len))
 
-plt.imshow(signal_2d)
-plt.show()
-
-plt.imshow(kernel_2d[0])
-plt.show()
-
-padded_kernel = np.ones(shape=(1, 2*side_len, side_len)) * -800
-padded_kernel[0, :side_len, :] = kernel_2d[0]
-
 test_img = vd.asrfftbuffer(signal_2d)
 kernel_img = vd.asrfftbuffer(kernel_2d)
 
-plt.imshow(np.abs(kernel_img.read_real(0)[0]))
-plt.colorbar()
-plt.savefig("kernel.png")
+vd.prepare_convolution_kernel(kernel_img)
 
-vd.prepare_convolution_kernel(kernel_img, shape=(1, side_len, side_len + 2))
-
-#plt.imshow(kernel_img.read_real(0)[0])
-#plt.colorbar()
-#plt.show()
-
-@vd.shader("out.size // 2")
-def do_mult(out: vc.Buffer[vc.c64], kernel: vc.Buffer[vc.c64]):
-    tid = vc.global_invocation().x
-
-    out[tid] = vc.mult_c64(out[tid], kernel[tid])
-
-#vd.rfft(test_img)
-
-#do_mult(test_img, kernel_img)
-
-#vd.irfft(test_img, normalize=True)
+output = vd.RFFTBuffer((side_len, side_len))
 
 # Perform an FFT on the buffer
-#vd.convolve_2d(test_img, kernel_img)
-
-#reference = vd.asrfftbuffer(signal_2d)
-#vd.rfft(reference) #, axes=[1])
+#vd.convolve_2d(output, input=test_img, kernel=kernel_img)
+vd.convolve_2d(test_img, kernel_img)
 
 #vd.convolve_2d(test_img, kernel_img, normalize=True)
 
-vd.rfft(test_img)
-
-result = np.abs(test_img.read_fourier(0))
+result = np.abs(test_img.read(0))
 #reference = cpu_convolve_2d(signal_2d, kernel_2d[0])/ side_len
 
+#reference = np.fft.rfft(signal_2d, axis=0)
+#reference = np.ascontiguousarray(reference).view(np.float32)
 
+reference = vd.asrfftbuffer(signal_2d)
+vd.rfft(reference)
+reference = reference.read(0)
 
-#reference = np.abs(reference.read_fourier(0))
-
-result = np.fft.ifftshift(result)
-reference = np.fft.ifftshift(reference)
+#result = np.fft.ifftshift(result)
+#reference = np.fft.ifftshift(reference)
 
 print(result.mean())
 print(reference.mean())
 
 #print((np.abs(result - reference)).mean())
 
-plt.imshow(np.abs(result - reference))
-plt.colorbar()
+fig, axs = plt.subplots(2, 2)
+
+# Plot the difference between result and reference
+axs[0, 0].imshow(result - np.abs(reference))
+axs[0, 0].set_title('Difference')
+axs[0, 0].set_xlabel('X')
+axs[0, 0].set_ylabel('Y')
+ 
+# Plot the absolute difference between result and reference
+axs[0, 1].imshow(np.abs(result - np.abs(reference)))
+axs[0, 1].set_title('Absolute Difference')
+axs[0, 1].set_xlabel('X')
+axs[0, 1].set_ylabel('Y')
+
+# Plot the reference
+axs[1, 0].imshow(np.abs(reference))
+axs[1, 0].set_title('Reference')
+axs[1, 0].set_xlabel('X')
+axs[1, 0].set_ylabel('Y')
+
+# Plot the result
+axs[1, 1].imshow(result)
+axs[1, 1].set_title('Result')
+axs[1, 1].set_xlabel('X')
+axs[1, 1].set_ylabel('Y')
+
 plt.show()
--- test2.py
+++ test2.py
@@ -1,39 +1,3 @@
-
-#data = [0.9127319, 0.7329998, 0.0011958987, 0.2081132, 0.56774485, 0.070007905, 0.18449862, 0.19299245, 0.26252803, 0.46872616, 0.03796457, 0.16031821, 0.5665843, 0.46797255, 0.9101622, 0.21621224, 0.034223225, 0.2636695, 0.42257962, 0.8933027, 0.83871573, 0.5227679, 0.8838187, 0.11580994, 0.9424848, 0.6193833, 0.51532704, 0.2923926, 0.17214341, 0.4877235, 0.3948569, 0.9867599, 0.5824371, 0.78653204, 0.2081846, 0.253086, 0.27474797, 0.16537598, 0.7246974, 0.7372597, 0.12605102, 0.73903614, 0.7202814, 0.05744897, 0.4580918, 0.028030781, 0.30317643, 0.2792883, 0.27650988, 0.5226246]
-
-data = list(range(50))
-
-print(sum(data)) # + sum(data[-2:]))
-
-import vkdispatch as vd
-
-vd.initialize(debug_mode=True)
-
-import vkdispatch.codegen as vc
-import numpy as np
-
-buff = vd.asbuffer(np.array(data, dtype=np.float32))
-
-@vd.reduce(0, group_size=1024 + 512)
-def sum_reduce(a: vc.f32, b: vc.f32) -> vc.f32:
-    return a + b
-
-print(sum_reduce)
-
-import time
-time.sleep(1)
-
-res_buf = sum_reduce(buff)
-
-time.sleep(0.5)
-
-print(res_buf.read(0), sum(data))
-
-time.sleep(0.5)
-
-
-exit()
-
 import vkdispatch as vd
 import vkdispatch.codegen as vc
 
@@ -43,43 +7,40 @@ from matplotlib import pyplot as plt
 
 vd.initialize(debug_mode=True)
 
-side_length = 624
+def make_random_complex_signal(shape):
+    r = np.random.random(size=shape)
+    i = np.random.random(size=shape)
+    return (r + i * 1j).astype(np.complex64)
 
-signal_shape = (side_length, side_length)
+def make_square_signal(shape):
+    signal = np.zeros(shape)
+    signal[shape[0]//4:3*shape[0]//4, shape[1]//4:3*shape[1]//4] = 1
+    return signal
 
-x = np.linspace(-1, 1, signal_shape[0])
-y = np.linspace(-1, 1, signal_shape[1])
-x, y = np.meshgrid(x, y)
-d = np.sqrt(x*x + y*y)
-sigma, mu = 0.02, 0.0
-gaussian_signal = np.exp(-((d - mu)**2 / (2.0 * sigma**2)))
+def make_gaussian_signal(shape):
+    x = np.linspace(-5, 5, shape[0])
+    y = np.linspace(-5, 5, shape[1])
+    xx, yy = np.meshgrid(x, y)
+    signal = np.exp(-xx**2 - yy**2)
+    return signal
 
-mid_idx = side_length // 2
+# Create a 2D buffer
+signal_2d = make_gaussian_signal((50, 50)) #make_random_complex_signal((50, 50))
 
-square_signal = np.zeros(signal_shape)
-square_signal[mid_idx - 64:mid_idx + 64, mid_idx - 64:mid_idx + 64] = 1
+test_img = vd.Buffer(signal_2d.shape, vd.complex64)
+test_img.write(signal_2d.astype(np.complex64))
 
-dot_signal = np.zeros(signal_shape, dtype=np.float32)
-dot_signal[mid_idx, mid_idx] = 1
+#test_img = vd.asbuffer(signal_2d.astype(np.complex64))
 
-input_signal = np.fft.fftshift(dot_signal).astype(np.float32)
-kernel_signal = np.fft.fftshift(square_signal).astype(np.float32)
+# Perform an FFT on the buffer
+vd.fft(test_img)
 
-convolved_signal = np.fft.ifftshift(np.fft.ifft2(
-    (np.fft.fft2(input_signal).astype(np.complex64) 
-    * np.fft.fft2(kernel_signal).astype(np.complex64))
-    .astype(np.complex64)))
-
-input_buffer = vd.asrfftbuffer(input_signal)
-kernel_buffer = vd.asrfftbuffer(kernel_signal.reshape(1, side_length, -1))
-output_buffer = vd.asrfftbuffer(np.ones(signal_shape).astype(np.float32))
-
-vd.prepare_convolution_kernel(kernel_buffer)
-vd.convolve_2d(input_buffer, kernel_buffer) #, input_buffer)
-
-result = np.fft.ifftshift(input_buffer.read_real(0))
-
-plt.imshow(result / side_length - np.abs(convolved_signal))
+plt.imshow(np.abs(test_img.read(0))) # - np.abs(np.fft.fft2(signal_2d)))
+plt.colorbar()
 plt.show()
 
-exit()
+#plt.imshow(np.abs(np.fft.fft2(signal_2d)))
+#plt.colorbar()
+#plt.show()
+
+#assert np.allclose(test_img.read(0), np.fft.fft2(signal_2d), atol=0.001)
