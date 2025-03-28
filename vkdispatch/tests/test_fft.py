c7f4aedabca745375a086c166eb764d180951cef
Shahar Sandhaus <wiishahar@gmail.com>
2025-03-28 13:23:21 -0700

    Added inverse fft

--- vkdispatch/tests/test_fft.py
+++ vkdispatch/tests/test_fft.py
@@ -84,3 +84,69 @@ def test_3d_fft():
             assert np.allclose(np.fft.fftn(data), test_data.read(0), atol=5e-2)
 
             current_shape[pick_dimention(dims)] *= random.choice([2, 3, 5, 7, 11, 13])
+
+def test_ifft_1d():
+    max_fft_size = vd.get_context().max_shared_memory // vd.complex64.item_size
+
+    max_fft_size = min(max_fft_size, vd.get_context().max_workgroup_size[0])
+
+    for _ in range(20):
+        dims = pick_dim_count(1)
+        current_shape = [pick_radix_prime() for _ in range(dims)]
+
+        while check_fft_dims(current_shape, max_fft_size):
+            print(current_shape)
+
+            data = np.random.rand(*current_shape).astype(np.complex64)
+            test_data = vd.Buffer(data.shape, vd.complex64)
+
+            for axis in range(dims):
+                test_data.write(data)
+
+                vd.fft.ifft(test_data, axis=axis)
+
+                assert np.allclose(np.fft.ifft(data, axis=axis), test_data.read(0), atol=1e-3)
+
+            current_shape[pick_dimention(dims)] *= random.choice([2, 3, 5, 7, 11, 13])
+
+def test_2d_ifft():
+    max_fft_size = vd.get_context().max_shared_memory // vd.complex64.item_size
+
+    max_fft_size = min(max_fft_size, vd.get_context().max_workgroup_size[0])
+
+    for _ in range(20):
+        dims = pick_dim_count(2)
+        current_shape = [pick_radix_prime() for _ in range(dims)]
+
+        while check_fft_dims(current_shape, max_fft_size):
+            data = np.random.rand(*current_shape).astype(np.complex64)
+            test_data = vd.Buffer(data.shape, vd.complex64)
+
+            test_data.write(data)
+
+            vd.fft.ifft2(test_data)
+
+            assert np.allclose(np.fft.ifft2(data), test_data.read(0), atol=1e-2)
+
+            current_shape[pick_dimention(dims)] *= random.choice([2, 3, 5, 7, 11, 13])
+
+def test_3d_ifft():
+    max_fft_size = vd.get_context().max_shared_memory // vd.complex64.item_size
+
+    max_fft_size = min(max_fft_size, vd.get_context().max_workgroup_size[0])
+
+    for _ in range(20):
+        dims = 3
+        current_shape = [pick_radix_prime() for _ in range(dims)]
+
+        while check_fft_dims(current_shape, max_fft_size):
+            data = np.random.rand(*current_shape).astype(np.complex64)
+            test_data = vd.Buffer(data.shape, vd.complex64)
+
+            test_data.write(data)
+
+            vd.fft.ifft3(test_data)
+
+            assert np.allclose(np.fft.ifftn(data), test_data.read(0), atol=5e-2)
+
+            current_shape[pick_dimention(dims)] *= random.choice([2, 3, 5, 7, 11, 13])
