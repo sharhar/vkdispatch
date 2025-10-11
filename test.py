7f6620ec25104fee12a3ab293652cfc4a9d1a3bf
Shahar Sandhaus <wiishahar@gmail.com>
2025-10-11 10:23:55 -0700

    Fixed RFFT bug

--- test.py
+++ test.py
@@ -52,15 +52,44 @@ def test_fft_1d():
 
     vd.fft.cache_clear()
 
-test_fft_1d()
+
+def test_rfft_1d():
+    max_fft_size = vd.get_context().max_shared_memory // vd.complex64.item_size
+
+    max_fft_size = min(max_fft_size, vd.get_context().max_workgroup_size[0])
+
+    for _ in range(20):
+        dims = pick_dim_count(1)
+        current_shape = [pick_radix_prime() for _ in range(dims)]
+
+        while check_fft_dims(current_shape, max_fft_size):
+            print(current_shape)
+
+            data = np.random.rand(*current_shape).astype(np.float32)
+            test_data = vd.RFFTBuffer(data.shape)
+
+            test_data.write_real(data)
+
+            vd.fft.rfft(test_data)
+
+            assert np.allclose(np.fft.rfft(data), test_data.read_fourier(0), atol=1e-3)
+
+            current_shape[pick_dimention(dims)] *= random.choice([2, 3, 5, 7, 11, 13])
+
+    vd.fft.cache_clear()
+
+
+test_rfft_1d()
 
 data = np.random.rand(495).astype(np.complex64)
-test_data = vd.Buffer(data.shape, vd.complex64)
+test_data = vd.RFFTBuffer(data.shape)
 #print(current_shape, axis)
 
-test_data.write(data)
+#test_data.write(data)
 
-vd.fft.fft(test_data, axis=0, print_shader=True)
+vd.fft.rfft(test_data) #, print_shader=True)
+
+exit()
 
 fft_data = test_data.read(0)
 np_data = np.fft.fft(data, axis=0)
