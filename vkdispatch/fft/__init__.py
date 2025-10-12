b988c3ff694642a9e454ea83442add4421b740c5
Shahar Sandhaus <wiishahar@gmail.com>
2025-10-12 11:35:46 -0700

    Added missing barrier

--- vkdispatch/fft/__init__.py
+++ vkdispatch/fft/__init__.py
@@ -6,8 +6,8 @@ from .io_manager import IOManager
 
 from .context import fft_context
 
-from .shader import make_fft_shader, get_cache_info, cache_clear, print_cache_info
-from .shader import make_convolution_shader
+from .shader_factories import make_fft_shader, get_cache_info, cache_clear, print_cache_info
+from .shader_factories import make_convolution_shader
 
 from .functions import fft, fft2, fft3, ifft, ifft2, ifft3
 from .functions import rfft, rfft2, rfft3, irfft, irfft2, irfft3
--- vkdispatch/tests/test_fft.py
+++ vkdispatch/tests/test_fft.py
@@ -4,7 +4,7 @@ import random
 
 from typing import List
 
-TEST_COUNT = 2
+TEST_COUNT = 20
 
 def numpy_convolution(signal: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     return np.fft.ifft2(
