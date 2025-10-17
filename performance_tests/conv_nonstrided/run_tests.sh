3d453e70a099fb40bccb39f73bc160d138fe8809
sharhar <wiishahar@gmail.com>
2025-10-17 18:45:15 +0000

    Fixed up convolution nonstrided graphs

--- performance_tests/conv_nonstrided/run_tests.sh
+++ performance_tests/conv_nonstrided/run_tests.sh
@@ -7,9 +7,9 @@ cd test_results
 DATA_SIZE=134217728
 #DATA_SIZE=67108864
 #DATA_SIZE=33554432
-ITER_COUNT=80
-BATCH_SIZE=10
-REPEATS=3
+ITER_COUNT=200
+BATCH_SIZE=20
+REPEATS=5
 
 /usr/local/cuda-12.0/bin/nvcc -O2 -std=c++17 ../conv_nonstrided_cufft.cu -gencode arch=compute_86,code=sm_86 -rdc=true -lcufft_static -lculibos -o conv_nonstrided_cufft.exec
 
@@ -19,18 +19,17 @@ echo "Iteration Count: $ITER_COUNT"
 echo "Batch Size: $BATCH_SIZE"
 echo "Repeats: $REPEATS"
 
-# echo "Running cuFFT FFT..."
-# ./conv_nonstrided_cufft.exec $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS
+echo "Running cuFFT FFT..."
+./conv_nonstrided_cufft.exec $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS
 
-# echo "Running Vkdispatch FFT..."
-# python3 ../conv_nonstrided_vkdispatch.py $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS
+echo "Running Vkdispatch FFT..."
+python3 ../conv_nonstrided_vkdispatch.py $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS
 
-# echo "Running PyTorch FFT..."
-# python3 ../conv_nonstrided_torch.py $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS
+echo "Running PyTorch FFT..."
+python3 ../conv_nonstrided_torch.py $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS
 
-# echo "Running ZipFFT FFT..."
-# python3 ../conv_nonstrided_zipfft.py $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS
+echo "Running ZipFFT FFT..."
+python3 ../conv_nonstrided_zipfft.py $DATA_SIZE $ITER_COUNT $BATCH_SIZE $REPEATS
 
 python3 ../conv_nonstrided_make_graph.py
-
 python3 ../conv_nonstrided_make_ratios_graph.py
