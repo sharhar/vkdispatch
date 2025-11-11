7bc5e9acb90f0f5089ea2b4561701817a30dbdee
Shahar Sandhaus <wiishahar@gmail.com>
2025-11-11 15:03:20 -0800

    Added batched vkfft convolutions for performance testing

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def numpy_convolution(signal: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     return np.fft.ifft2(
         np.fft.fft2(signal).astype(np.complex64)
         *
-        np.fft.fft2(kernel).astype(np.complex64).conjugate()
+        np.fft.fft2(kernel).astype(np.complex64) # .conjugate()
     )
 
 
@@ -32,13 +32,13 @@ def save_signal(filename: str, data: np.ndarray):
     for ii, layer in enumerate(data):
         np.save(f"data/{filename}_layer{ii}.npy", layer)
 
-current_shape = (2, 128, 128)
+current_shape = (2, 11, 5)
 
 #data = np.random.rand(*current_shape).astype(np.complex64)
 #data2 = np.random.rand(*current_shape).astype(np.complex64)
 
-data = make_circle_signal(current_shape, 20).astype(np.complex64)
-data2 = make_square_signal(current_shape, 15).astype(np.complex64)
+data = np.array([make_circle_signal(current_shape[1:], 10 * (i + 1)) for i in range(current_shape[0])]).astype(np.complex64)
+data2 = np.array([make_square_signal(current_shape[1:], 50 * (i + 1)) for i in range(current_shape[0])]).astype(np.complex64)
 
 save_signal("input_signal", data)
 save_signal("kernel_signal", data2)
@@ -46,18 +46,19 @@ save_signal("kernel_signal", data2)
 test_data = vd.asbuffer(data)
 kernel_data = vd.asbuffer(data2)
 
-vd.fft.fft2(kernel_data)
+#    vd.fft.fft2(kernel_data)
+
 
 #np.save("ffted_kernel.npy", kernel_data.read(0))
 #np.save("ffted_kernel_reference.npy", np.fft.fft2(data2).astype(np.complex64))
 
-kernel_transposed = vd.fft.transpose(kernel_data, axis=0, print_shader=True)
+#kernel_transposed = vd.fft.transpose(kernel_data, axis=1)
+#vd.fft.convolve2D(test_data, kernel_transposed, transposed_kernel=True)
 
-#np.save("transposed_kernel.npy", kernel_transposed.read(0).reshape(275, -1))
+#vd.fft.convolve2D(test_data, kernel_transposed, transposed_kernel=True)
 
-vd.fft.fft(test_data)
-vd.fft.convolve(test_data, kernel_transposed, axis=0, transposed_kernel=True) #, print_shader=True)
-vd.fft.ifft(test_data)
+vd.vkfft.transpose_kernel2D(kernel_data)
+vd.vkfft.convolve2D(test_data, kernel_data, normalize=True)
 
 save_signal("convolved_signal", test_data.read(0))
 save_signal("convolved_signal_fourier", np.fft.fft2(test_data.read(0)))
@@ -66,5 +67,8 @@ reference_data = numpy_convolution(data, data2)
 
 save_signal("reference_convolved_signal", reference_data)
 save_signal("reference_convolved_signal_fourier", np.fft.fft2(reference_data))
+
+save_signal("difference_convolved_signal", reference_data - test_data.read(0))
+save_signal("difference_convolved_signal_fourier", np.fft.fft2(reference_data) - np.fft.fft2(test_data.read(0)))
 
 assert np.allclose(reference_data, test_data.read(0), atol=1e-3)
