ce401c3046f5c5a01827ceaa778bdbc6aa275f30
Shahar Sandhaus <wiishahar@gmail.com>
2025-11-11 12:46:01 -0800

    Working towards vkfft convolution

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import vkdispatch as vd
+import vkdispatch.codegen as vc
 import numpy as np
-import random
 
-from typing import List
+SIZE = 2 ** 6
 
 def numpy_convolution(signal: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     return np.fft.ifft2(
@@ -11,68 +11,60 @@ def numpy_convolution(signal: np.ndarray, kernel: np.ndarray) -> np.ndarray:
         np.fft.fft2(kernel).astype(np.complex64).conjugate()
     )
 
-def pick_radix_prime():
-    return random.choice([2, 3, 5, 7, 11, 13])
 
-def pick_dim_count(min_dim):
-    return random.choice(list(range(min_dim, 4)))
+def make_circle_signal(shape, radius):
+    center = (shape[0] // 2, shape[1] // 2)
+    Y, X = np.ogrid[:shape[0], :shape[1]]
+    dist_from_center = np.sqrt((X - center[1])**2 + (Y - center[0])**2)
+    mask = dist_from_center <= radius
+    array = np.zeros(shape, dtype=np.float32)
+    array[mask] = 1.0
+    return array
 
-def pick_dimention(dims: int):
-    if dims == 1:
-        return 0
+def make_square_signal(shape, size):
+    array = np.zeros(shape, dtype=np.float32)
+    start_x = (shape[1] - size) // 2
+    start_y = (shape[0] - size) // 2
+    array[start_y:start_y + size, start_x:start_x + size] = 1.0
+    return array
 
-    return random.choice(list(range(dims)))
+def save_signal(filename: str, data: np.ndarray):
+    for ii, layer in enumerate(data):
+        np.save(f"data/{filename}_layer{ii}.npy", layer)
 
-def check_fft_dims(fft_dims: List[int], max_fft_size: int):
-    return all([dim <= max_fft_size for dim in fft_dims]) and np.prod(fft_dims) * vd.complex64.item_size < 2 ** 20
+current_shape = (2, 128, 128)
 
+#data = np.random.rand(*current_shape).astype(np.complex64)
+#data2 = np.random.rand(*current_shape).astype(np.complex64)
 
-def test_convolution_2d_transpose():
-    max_fft_size = vd.get_context().max_shared_memory // vd.complex64.item_size
+data = make_circle_signal(current_shape, 20).astype(np.complex64)
+data2 = make_square_signal(current_shape, 15).astype(np.complex64)
 
-    max_fft_size = min(max_fft_size, vd.get_context().max_workgroup_size[0])
-
-    for _ in range(5):
-        dims = pick_dim_count(2)
-        current_shape = [pick_radix_prime() for _ in range(dims)]
-
-        while check_fft_dims(current_shape, max_fft_size):
-            print("Testing convolution 2D transpose with shape:", current_shape)
-            
-            data = np.random.rand(*current_shape).astype(np.complex64)
-            data2 = np.random.rand(*current_shape).astype(np.complex64)
-
-            test_data = vd.asbuffer(data)
-            kernel_data = vd.asbuffer(data2)
-
-            vd.fft.fft2(kernel_data)
-            kernel_transposed = vd.fft.transpose(kernel_data, axis=len(kernel_data.shape)-2)
-            vd.fft.convolve2D(test_data, kernel_transposed, transposed_kernel=True)
-
-            reference_data = numpy_convolution(data, data2)
-
-            assert np.allclose(reference_data, test_data.read(0), atol=1e-3)
-
-            current_shape[pick_dimention(dims)] *= random.choice([2, 3, 5, 7, 11, 13])
-    
-    vd.fft.cache_clear()
-
-
-#test_convolution_2d_transpose()
-
-#test_fft_1d()
-
-#data = np.random.rand(11, 2, 5).astype(np.complex64)
-data = np.random.rand(11, 2, 5).astype(np.complex64)
-data2 = np.random.rand(11, 2, 5).astype(np.complex64)
+save_signal("input_signal", data)
+save_signal("kernel_signal", data2)
 
 test_data = vd.asbuffer(data)
 kernel_data = vd.asbuffer(data2)
 
 vd.fft.fft2(kernel_data)
-#kernel_transposed = vd.fft.transpose(kernel_data, axis=len(kernel_data.shape)-2)
-vd.fft.convolve2D(test_data, kernel_data, print_shader=True) #, transposed_kernel=True)
+
+#np.save("ffted_kernel.npy", kernel_data.read(0))
+#np.save("ffted_kernel_reference.npy", np.fft.fft2(data2).astype(np.complex64))
+
+kernel_transposed = vd.fft.transpose(kernel_data, axis=0, print_shader=True)
+
+#np.save("transposed_kernel.npy", kernel_transposed.read(0).reshape(275, -1))
+
+vd.fft.fft(test_data)
+vd.fft.convolve(test_data, kernel_transposed, axis=0, transposed_kernel=True) #, print_shader=True)
+vd.fft.ifft(test_data)
+
+save_signal("convolved_signal", test_data.read(0))
+save_signal("convolved_signal_fourier", np.fft.fft2(test_data.read(0)))
 
 reference_data = numpy_convolution(data, data2)
+
+save_signal("reference_convolved_signal", reference_data)
+save_signal("reference_convolved_signal_fourier", np.fft.fft2(reference_data))
 
 assert np.allclose(reference_data, test_data.read(0), atol=1e-3)
---
+++ tests/test_vkfft_conv.py
@@ -0,0 +1,61 @@
+import vkdispatch as vd
+import random
+
+from typing import List
+import numpy as np
+
+def numpy_convolution_1d(signal: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    return np.fft.ifft(
+        np.fft.fft(signal).astype(np.complex64)
+        *
+        np.fft.fft(kernel).astype(np.complex64).conjugate()
+    )
+
+def numpy_convolution(signal: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    return np.fft.ifft2(
+        np.fft.fft2(signal).astype(np.complex64)
+        *
+        np.fft.fft2(kernel).astype(np.complex64).conjugate()
+    )
+
+def pick_radix_prime():
+    return random.choice([2, 3, 5, 7, 11, 13])
+
+def pick_dim_count(min_dim):
+    return random.choice(list(range(min_dim, 4)))
+
+def pick_dimention(dims: int):
+    if dims == 1:
+        return 0
+
+    return random.choice(list(range(dims)))
+
+def check_fft_dims(fft_dims: List[int], max_fft_size: int):
+    return all([dim <= max_fft_size for dim in fft_dims]) and np.prod(fft_dims) * vd.complex64.item_size < 2 ** 20
+
+def test_convolution_2d():
+    max_fft_size = vd.get_context().max_shared_memory // vd.complex64.item_size
+
+    max_fft_size = min(max_fft_size, vd.get_context().max_workgroup_size[0])
+
+    for _ in range(4):
+        dims = pick_dim_count(2)
+        current_shape = [pick_radix_prime() for _ in range(dims)]
+
+        while check_fft_dims(current_shape, max_fft_size):
+            data = np.random.rand(*current_shape).astype(np.complex64)
+            data2 = np.random.rand(*current_shape).astype(np.complex64)
+
+            test_data = vd.asbuffer(data)
+            kernel_data = vd.asbuffer(data2)
+
+            #vd.fft.fft2(kernel_data)
+            #vd.fft.convolve2D(test_data, kernel_data)
+
+            reference_data = numpy_convolution(data, data2)
+
+            assert np.allclose(reference_data, test_data.read(0), atol=1e-3)
+
+            current_shape[pick_dimention(dims)] *= random.choice([2, 3, 5, 7, 11, 13])
+    
+    vd.fft.cache_clear()
